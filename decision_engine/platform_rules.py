a639e7e2c1deb59fedec8aa98fc9f8105537da2d
gaur3009-p <gaur3009@gmail.com>
2026-01-22 01:30:56 +0530

    Create platform_rules.py

---
+++ decision_engine/platform_rules.py
@@ -0,0 +1,17 @@
+PLATFORM_RULES = {
+    "Meta": {
+        "emotion_weight": 0.5,
+        "visual_weight": 0.3,
+        "clarity_weight": 0.2
+    },
+    "Google": {
+        "emotion_weight": 0.2,
+        "visual_weight": 0.2,
+        "clarity_weight": 0.6
+    },
+    "LinkedIn": {
+        "emotion_weight": 0.3,
+        "visual_weight": 0.2,
+        "clarity_weight": 0.5
+    }
+}
