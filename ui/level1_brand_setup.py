01d69e8834d3109e9818429f9c9df5aabcf90bbe
gaur3009-p <gaur3009@gmail.com>
2026-01-22 01:36:54 +0530

    Create level1_brand_setup.py

---
+++ ui/level1_brand_setup.py
@@ -0,0 +1,10 @@
+def level1_inputs():
+    return [
+        "Brand Name",
+        "Brand Description",
+        "Industry",
+        "Target Audience",
+        "Campaign Objective",
+        "Brand Tone",
+        "Platforms"
+    ]
